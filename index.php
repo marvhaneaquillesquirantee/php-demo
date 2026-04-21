a0294d6bf0e35e8d5ab2d295cdbd8afb7c379073
Marvhane Aquilles <marvhaneaquilles.quirante@cvsu.edu.ph>
2026-04-21 19:48:52 +0800

    feat: navbar, lab1 - 3 logic code

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+    
+<head>
+   
+    <title>MAIN</title>
+</head>
+<style>
+    .navbar {
+        background: rgba(255, 230, 240, 0.8);
+        backdrop-filter: blur(5px);
+        background-color: #ffe6f0; /* light pink */
+        padding: 10px 20px;
+        display: flex;
+        justify-content: center;
+        border-bottom: 2px solid #ff99c8;
+    
+    }
+
+    .navbar a {
+        color: #b30059;
+        text-decoration: none;
+        margin-left: 20px;
+        font-size: 18px;
+        padding: 6px 10px;
+        border-radius: 6px;
+        transition: 0.3s;
+    }
+
+    .navbar a:hover {
+       background-color: #ffb3d1;
+        color: white;
+    }
+</style>
+
+
+<body>
+    <div class="navbar">
+    <a href="lab1.php">Laboratory 1</a>
+    <a href="lab2.php">Laboratory 2</a>
+    <a href="lab3.php">Laboratory 3</a>
+</div>
+</body>
+</html>
